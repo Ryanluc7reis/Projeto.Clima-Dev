3b8e0fce7863734b6241d198560eaac17739e853
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-05-04 23:21:20 -0300

    Melhorado index

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 import BackgroundWithChildren from '../src/components/layout/backgroundWithChildren/BackgroundWithChildren'
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: ${props => props.theme.white};
+  min-width: 400px;
+
+  @media (max-width: 500px){
+    min-width: 100%;
+  }
 `
 
 export default function HomePage() {
