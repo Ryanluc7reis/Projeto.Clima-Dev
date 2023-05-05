fd048fdf0f941dae5c2755c3ed942c6fc7a120cf
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2023-05-04 23:16:13 -0300

    Criado background image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
+import BackgroundWithChildren from '../src/components/layout/backgroundWithChildren/BackgroundWithChildren'
 
 const StyledDiv = styled.div`
   background-color: red;
 `
 
 export default function HomePage() {
-  return <StyledDiv>Home Page</StyledDiv>
+  return (
+    <BackgroundWithChildren>
+      <StyledDiv>Home Page</StyledDiv>
+    </BackgroundWithChildren>
+    
+  ) 
 }
